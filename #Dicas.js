3520732b4c068ae67126e0a859b5f0660280fe02
Bruno Santos <84uno.santos@gmail.com>
2025-10-18 10:06:19 -0300

    Criando Pasta Laços

--- #Dicas.js
+++ #Dicas.js
@@ -1,3 +1,6 @@
+/** 
+ * Documentação básica de JavaScript
+ */
 // case sensitive
 /* comentário trecho */ 
 // comentário linha
@@ -10,3 +13,9 @@ prompt // abre janela pra entrada do usuário
 
 // no html é boa prática colocar o script no final do body
 <script src="script.js"></script>
+
+
+/* CTRL ' Abrir prompt no VS Code */
+node -v // verificar versão do node
+// Deve ser executa na pasta onde está o arquivo .js
+node script.js // executar script.js no node
---
+++ Lacos/condicionais.js
@@ -0,0 +1,11 @@
+/**
+ * Exemplos de uso de condicionais em JavaScript
+ */
+let idade = 16;
+//Usando if(se)
+if (idade >= 18) {
+    console.log("Você pode dirigir!.");
+}
+else {
+    console.log("Menor de idade NÃO pode dirigir!.");
+}
