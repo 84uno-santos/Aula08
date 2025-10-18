0be3f184428c9c24d8c090994ab6a420c0d55839
Bruno Santos <84uno.santos@gmail.com>
2025-10-18 11:10:43 -0300

    Dias JS

--- #Dicas.js
+++ #Dicas.js
@@ -19,3 +19,43 @@ prompt // abre janela pra entrada do usuário
 node -v // verificar versão do node
 // Deve ser executa na pasta onde está o arquivo .js
 node script.js // executar script.js no node
+
+/** 
+ * Operadores Relacionais
+ * > maior              10 > 5   => verdadeiro
+ * < menor              5 < 2    => falso
+ * == igual             5 == '5' => verdadeiro
+ * != diferente         5 != 3   => verdadeiro
+ * >= maior ou igual    5 >= 5   => verdadeiro
+ * <= menor ou igual    3 <= 2   => falso
+ * 
+ * Comparadores de texto 
+ * 'Bruno' == 'Bruno'    => verdadeiro
+ * 'Bruno' != 'bruno'    => verdadeiro (case sensitive)
+ * 'A' > 'B'             => falso (ordem alfabética)
+ * 'AB' > 'AA'           => verdadeiro (ordem alfabética)   
+ * 'Ana' > 'ana'       => falso (case sensitive)    
+ *
+ */ 
+
+/** 
+ * Operadores Lógicos
+ * && e (and)        idade >= 18 && idade < 65  => true para idades entre 18 e 64
+ * || ou (or)        idade < 18 || idade > 60   => true para menores de 18 ou maiores de 60
+ * ! não (not)       !(idade >=18)              => true para menores de 18
+ */
+
+/**
+
+ */
+
+/** 
+ * Operadores Aritméticos
+ * + adição / concatenação
+ * - subtração      
+ * * multiplicação
+ * / divisão    
+ * % resto da divisão
+ * **  potência
+ */
+
